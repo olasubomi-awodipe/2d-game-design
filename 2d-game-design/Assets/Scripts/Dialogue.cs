d69d9594bc2fb6052cd9bd728339dcab5837e806
Olasubomi Awodipe <subomiawodipe1@gmail.com>
2024-06-16 18:50:16 -0700

    Created a dialogue system

--- 2d-game-design/Assets/Scripts/Dialogue.cs
+++ 2d-game-design/Assets/Scripts/Dialogue.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dialogue : MonoBehaviour
+[System.Serializable]
+public class Dialogue 
 {
-    
+    public string name;
+
+    [TextArea(3, 10)]
+    public string[] sentences;
 }
--- 2d-game-design/Assets/Scripts/DialogueManager.cs
+++ 2d-game-design/Assets/Scripts/DialogueManager.cs
@@ -1,8 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-   
+
+    //
+    public TMP_Text nameText;
+
+    public TMP_Text dialogueText;
+
+    private Queue<string> sentences;
+    // Use this for initialization
+   void Start ()
+    {
+        // initialize the queue storing the sentences
+        sentences = new Queue<string>();
+    }
+
+    // create a method to start the conversation
+    public  void StartDialogue (Dialogue dialogue)
+    {
+        // 
+        nameText.text = dialogue.name;
+
+        // clear the previous sentence before the start of the new sentence
+        sentences.Clear();
+
+        // iterate through each sentence in dialogue
+        foreach (string sentence in dialogue.sentences)
+        {
+            // add a new sente
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    // create a method to display the next sentence
+    public void DisplayNextSentence ()
+    {
+        // check if there is any sentence left in the current dialogue
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        // 
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;  
+
+        void EndDialogue ()
+        {
+            Debug.Log("End of conversation.");
+        }
+    }
 }
